2c4a4e1ef1d8f03fc148cdb46e716837f9eb4506
abhinavkk <keshariabhinavk1996@gmail.com>
2016-03-12 01:59:01 +0530

    changing directory of main page

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-      io.emit('chat message', msg);
+      socket.broadcast.emit('chat message', msg);
       console.log('message: ' + msg);
     });
 });
